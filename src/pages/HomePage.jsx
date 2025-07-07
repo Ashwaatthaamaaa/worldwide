8734c58557007726bb20c9fbbd32dceab8b509f8
Sarthak Saraswat <sarthaksaraswat298@gmail.com>
2025-07-08 05:04:46 +0530

    linked routes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Homepage</h1>
+      {/* <a href="/pricing">Pricing</a> */}
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
 
 export default HomePage;
