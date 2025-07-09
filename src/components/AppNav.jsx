4f8af969c9f99b7e0b543c0d366428698c66f04a
Sarthak Saraswat <sarthaksaraswat298@gmail.com>
2025-07-10 04:01:31 +0530

    added cities and countries nested routes

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,21 @@
+import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 
 function AppNav() {
-  return <nav className={styles.nav}>AppNav</nav>;
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>{" "}
+          {/*routing works relatively, this
+          wont work if used outside /app route*/}
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default AppNav;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,5 +1,15 @@
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
+
+import styles from "./AppLayout.module.css";
+
 function AppLayout() {
-  return <div>App</div>;
+  return (
+    <div className={styles.app}>
+      <Sidebar /> {/* not doing any composition */}
+      <Map />
+    </div>
+  );
 }
 
 export default AppLayout;
